1132274f38cf4836336940bc2c1cd038bcd7fc8d
罗玉林 <luoyulin1@jd.com>
2018-02-07 16:22:40 +0800

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Created by luoyulin1 on 2018/2/7.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+
+
+}
---
+++ src/main/java/com/neo/majiang/people/GamePlayer.java
@@ -0,0 +1,33 @@
+package com.neo.majiang.people;
+
+/**
+ * Íæ¼Ò
+ * Created by luoyulin1 on 2018/2/7.
+ */
+public class GamePlayer {
+
+    /**
+     * ÊÖÅÆ
+     */
+    private Integer [] handValue;
+
+    /**
+     * ³öÅÆ
+     * @param v
+     */
+    public void out(Integer v) {
+
+    }
+
+    /**
+     * ÃşÅÆ
+     * @param v
+     */
+    public void in(Integer v) {
+
+    }
+
+
+}
+
+
